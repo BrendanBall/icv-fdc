a857c661d06f536c29e050d09614ef3a6c2f4a51
Brendan Ball <ball.brendan50@gmail.com>
2015-08-12 09:50:50 +0200

    changed array structure to 2d

--- edge_detection.py
+++ edge_detection.py
@@ -1,14 +1,6 @@
+def gaussian_blur(img_array, img_size):
+	output_array = [None] * len(img_array)
+	for i in range(len(img_array)):
 
-class EdgeDetection:
-	def __init__(self, img_array):
-		self.img_array = img_array
-
-
-	def gaussian_blur(self, window_size):
-		gaussian = [ 0.05,0.05,0.05,
-					 0.05,0.6, 0.05, 
-					 0.05,0.05,0.05]
-		output_array = [None] * len(self.img_array)
-		for i in range(len(self.img_array)):
-			output_array[i] = self.img_array[i] * 2
-		return output_array
+		output_array[i] = img_array[i] * 2
+	return img_array
--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from PIL import Image
 import PIL
 import sys
 import numpy as np
-from edge_detection import EdgeDetection
+from edge_detection import gaussian_blur
 
 
 if __name__ == "__main__":
 	im = Image.open(sys.argv[1]).convert("L")
-	img_array = list(im.getdata())
 	im.show()
-	edge_det = EdgeDetection(img_array)
-	blurred_array = edge_det.gaussian_blur(3) 
+	img_array = list(im.getdata())
+	print im.size
+	width = im.size[0]
+	img_array = [img_array[i:i+width] for i in range(0, len(img_array), width)]
+	print (len(img_array[0]), len(img_array))
+	blurred_array = gaussian_blur(img_array, im.size) 
 	blurred_img = Image.new("L", im.size)
-	blurred_img.putdata(blurred_array)
+	blurred_img.putdata([x+y for x,y in blurred_array])
 	blurred_img.show()
 
