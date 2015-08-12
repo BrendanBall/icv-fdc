c3949e898d5a249ddd166caeba477597ad524b7e
Brendan Ball <ball.brendan50@gmail.com>
2015-08-12 23:24:43 +0200

    found weird hacky edge detection for patricks sample images using numpy

--- edge_detection.py
+++ edge_detection.py
@@ -1,6 +1,17 @@
-def gaussian_blur(img_array, img_size):
-	output_array = [None] * len(img_array)
-	for i in range(len(img_array)):
+import numpy as np 
 
-		output_array[i] = img_array[i] * 2
-	return img_array
+def gaussian_blur(img_array, img_size):
+	output_array = [[0]*img_size[0]] * img_size[1]
+	for y in range(img_size[1]):
+		for x in range(img_size[0]):
+			output_array[y][x] = img_array[y][x] * 2
+
+	return output_array
+
+def weird_hack(img_array, img_size):
+	output_array = np.array(img_array, np.int8)
+	for y in range(img_size[1]):
+		for x in range(img_size[0]):
+			output_array[y][x] = int(img_array[y][x] *2)
+
+	return output_array
--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from PIL import Image
 import PIL
 import sys
 import numpy as np
-from edge_detection import gaussian_blur
+from edge_detection import weird_hack
 
 
-if __name__ == "__main__":
+def weird_edgedet():
 	im = Image.open(sys.argv[1]).convert("L")
 	im.show()
 	img_array = list(im.getdata())
 	print im.size
 	width = im.size[0]
 	img_array = [img_array[i:i+width] for i in range(0, len(img_array), width)]
-	print (len(img_array[0]), len(img_array))
-	blurred_array = gaussian_blur(img_array, im.size) 
+	blurred_array = weird_hack(img_array, im.size) 
+	blurred_array = [item for sublist in blurred_array for item in sublist]
 	blurred_img = Image.new("L", im.size)
-	blurred_img.putdata([x+y for x,y in blurred_array])
+	blurred_img.putdata(blurred_array)
 	blurred_img.show()
 
+if __name__ == "__main__":
+	weird_edgedet()
