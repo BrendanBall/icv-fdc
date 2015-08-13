ab1beb1432a8bfedb5ae83dd3b6a9d987bbaa511
Brendan Ball <ball.brendan50@gmail.com>
2015-08-13 22:47:12 +0200

    added gaussian blur

--- edge_detection.py
+++ edge_detection.py
@@ -1,17 +1,44 @@
 import numpy as np 
+from math import pi,e
 
-def gaussian_blur(img_array, img_size):
-	output_array = [[0]*img_size[0]] * img_size[1]
+
+def G(x, y, variance=0.2):
+	gaussian = [[0.1,0.1,0.1],[0.1,0.5,0.1],[0.1,0.1,0.1]]
+	return (1/ (2 * pi * variance)) * e ** (- (x**2 + y**2)/(2 * variance))
+
+def gaussian_kernel(img_array, img_size, x, y, kernelsize):
+	sum = 0;
+	for j in range(-kernelsize, kernelsize+1):
+		for i in range(-kernelsize, kernelsize+1):
+			if (y + j >= 0 and x + i >= 0 and y + j < img_size[1]  and x + i < img_size[0]):
+				sum += img_array[y + j][x + i] * G(i, j)
+				#print i+x,j+y, img_array[y + j][x + i] * G(i, j), G(i, j)
+
+	#print sum
+	#if sum > 0:
+		#print sum
+	return int(sum)
+
+
+
+
+
+def gaussian_blur(img_array, img_size, kernelsize):
+	output_array = np.copy(img_array)
 	for y in range(img_size[1]):
 		for x in range(img_size[0]):
-			output_array[y][x] = img_array[y][x] * 2
+
+			output_array[y][x] = gaussian_kernel(img_array, img_size, x, y, kernelsize)
+
 
 	return output_array
+
+
 
 def weird_hack(img_array, img_size):
 	output_array = np.array(img_array, np.int8)
 	for y in range(img_size[1]):
 		for x in range(img_size[0]):
-			output_array[y][x] = int(img_array[y][x] *2)
+			output_array[y][x] = int(img_array[y][x] * 2)
 
 	return output_array
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PIL import Image
 import PIL
 import sys
 import numpy as np
-from edge_detection import weird_hack
+from edge_detection import *
 
 
 def weird_edgedet():
@@ -19,4 +19,13 @@ def weird_edgedet():
 	blurred_img.show()
 
 if __name__ == "__main__":
-	weird_edgedet()
+	img = Image.open(sys.argv[1]).convert("L")
+	img.show()
+	img_array = np.array(img, np.uint8).reshape(img.size[1], img.size[0])
+	#temp = np.array([[0,100,50],[20,150,75],[50,0,100]], np.uint8)
+	blurred_array = gaussian_blur(img_array, img.size, 1)
+	#print temp
+	#print blurred_array
+	blurred_array = weird_hack(blurred_array, img.size)
+	blurred_img = Image.fromarray(np.uint8(blurred_array))
+	blurred_img.show()
